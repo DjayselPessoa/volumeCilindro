ab088a96daaf1a17036f2d4f924160bbbb11bf14
Djaysel Pessôa <djaysel@outlook.com>
2021-08-19 16:56:22 -0300

    Create volumeCilindro.py

---
+++ volumeCilindro.py
@@ -0,0 +1,23 @@
+ativo = True
+
+while ativo:
+    try:
+        print("Informe agora os dados para obtenção do volume do cilindro: ")
+        raioCilindro = float(input("raio(r) -> "))
+        alturaCilindro = float(input("altura(h) -> "))
+
+        volume = (3.14 * (raioCilindro ** 2)) * alturaCilindro
+        print(f"Volume do Cilindro: 3.14 * r² * h = {volume}³")
+
+        onOff = str(input("S para sair - C para continuar: "))
+        if onOff == "S" or onOff == "s":
+            print("Saindo do programa!")
+            ativo = False
+            # você pode usar o break aq no lugar de mudar pra False
+        elif onOff == "C" or onOff == "c":
+            print("Reiniciando!")
+            ativo = True
+        else:
+            raise ValueError("Escolha S ou C")
+    except ValueError as e:
+        print("Erro", e)
